1d81479ff3d655c3b4fc95dff7f7a86b5039f8ae
Pavel Orlovskyi <Lorengreyn@gmail.com>
2022-02-01 21:16:02 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from "SimpleLightbox";
+import "SimpleLightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryList = document.querySelector(`.gallery`);
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => { 
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+  </a>
+  </li>`;
+  })
+    .join("");
+  
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
 
 console.log(galleryItems);
+
