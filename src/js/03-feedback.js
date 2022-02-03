bca99cb5b989f5f96647095ebc51db5f044d16eb
Pavel Orlovskyi <Lorengreyn@gmail.com>
2022-02-03 19:59:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('form');
+const email = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const localStorageKey = 'feedback-form-state';
+
+updateForm();
+form.addEventListener('submit', submitData);
+form.addEventListener('input', throttle(saveData, 500));
+
+function saveData() {
+  const data = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(data));
+}
+
+function updateForm() {
+  const getData = JSON.parse(localStorage.getItem(localStorageKey));
+  email.value = getData?.email || '';
+  textarea.value = getData?.message || '';
+}
+
+function submitData(event) {
+  event.preventDefault();
+
+  const getData = JSON.parse(localStorage.getItem(localStorageKey));
+
+  console.log(getData);
+
+  form.reset();
+  localStorage.clear();
+}
