c46f164872f0e3242dbc7c3400ee28df79ef0baf
Pavel Orlovskyi <Lorengreyn@gmail.com>
2022-02-03 19:27:02 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const localStorageKey = 'videoplayer-current-time';
+
+const currentTime = JSON.parse(localStorage.getItem(localStorageKey));
+//Set the current playback position in seconds.
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+//Get the current playback position in seconds.
+player.on('timeupdate', throttle(timeSave, 800));
+function timeSave() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem(localStorageKey, JSON.stringify(seconds));
+  });
+}
